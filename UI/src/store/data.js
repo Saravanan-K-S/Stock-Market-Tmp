ee07fdd735c388939fe245d4fc40bf1f261c8c7a
Saravanan K S <saravananks@presidio.com>
2024-10-01 12:43:46 +0530

    feat: updated mock data

--- UI/src/store/data.js
+++ UI/src/store/data.js
@@ -1,4 +1,3 @@
-// Exporting a constant array named STOCK_LIST which contains a list of stock objects
 export const STOCK_LIST = [
     {
         id: 1, // Unique identifier for the stock
@@ -14,5 +13,30 @@ export const STOCK_LIST = [
         id: 3, // Unique identifier for the stock
         name: 'Amazon.com Inc.', // Name of the company
         price: 3372.20, // Current price of the stock
+    },
+    {
+        id: 4, // Unique identifier for the stock
+        name: 'Google LLC', // Name of the company
+        price: 2800.50, // Current price of the stock
+    },
+    {
+        id: 5, // Unique identifier for the stock
+        name: 'Facebook, Inc.', // Name of the company
+        price: 355.64, // Current price of the stock
+    },
+    {
+        id: 6, // Unique identifier for the stock
+        name: 'Tesla, Inc.', // Name of the company
+        price: 730.17, // Current price of the stock
+    },
+    {
+        id: 7, // Unique identifier for the stock
+        name: 'Netflix, Inc.', // Name of the company
+        price: 590.65, // Current price of the stock
+    },
+    {
+        id: 8, // Unique identifier for the stock
+        name: 'NVIDIA Corporation', // Name of the company
+        price: 220.42, // Current price of the stock
     }
 ]
